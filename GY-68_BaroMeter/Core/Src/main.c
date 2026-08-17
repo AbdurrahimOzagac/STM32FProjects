3c29c2790922f25af8c14ef22534b870a86b8974
goksel-esetron <60106865+goksel-esetron@users.noreply.github.com>
2026-08-17 09:56:26 +0300

    new pc init

--- GY-68_BaroMeter/Core/Src/main.c
+++ GY-68_BaroMeter/Core/Src/main.c
@@ -60,9 +60,9 @@ typedef struct {
 	int16_t AC1;
 	int16_t AC2;
 	int16_t AC3;
-	uint16_t AC4; // Dikkat: Bunlar Unsigned!
-	uint16_t AC5; // Dikkat: Bunlar Unsigned!
-	uint16_t AC6; // Dikkat: Bunlar Unsigned!
+	uint16_t AC4;
+	uint16_t AC5;
+	uint16_t AC6;
 	int16_t B1;
 	int16_t B2;
 	int16_t MB;
